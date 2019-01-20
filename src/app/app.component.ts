1ca8bd7d1eef6597ee00de30424f7137b1d60503
liciolentimo <lentimo@gmail.com>
2019-01-20 17:47:57 +0300

    Added routing links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user';
+import { ProfileService } from './profile.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Gitsearch';
+
+  user:User;
+  profile:any[];
+  repos =[];
+  username:string;
+
+  constructor(private profileService:ProfileService) { }
+
+    searchUser(){
+    // this.profileService.updateProfile(this.username);
+  	// this.profileService.getProfileInfo(name).subscribe(profile => {
+    //   console.log(profile);
+    // });
+    // this.profileService.getProfileRepos(name).subscribe(repos => {
+    //   console.log(repos);
+    // })
+  }
+  
+  ngOnInit() {
+  }
 }
