31722c45fdc95e48089cb7f6f1918b0d16f4eb89
liciolentimo <lentimo@gmail.com>
2019-01-20 17:46:06 +0300

    Added getProfileInfo() method

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,21 +1,99 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from './user';
+import { Repository } from './repository';
+
 // import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  complete:boolean = false;
 
-  private username:string;
-  private clientid = 'a427cef0d946c7f7df23';
-  private clientsecret = '9a2bab196c8c42c6984eb0f85c68bcac01e04556';
+  // private username:string;
+  // private clientid = 'a427cef0d946c7f7df23';
+  // private clientsecret = '9a2bab196c8c42c6984eb0f85c68bcac01e04556';
 
-  constructor(private http:HttpClient) {
-    this.username = 'liciolentimo';
-   }
-   getProfile(){
-      return this.http.get("https://api.github.com/users/liciolentimo?access_token=6021f4f77f5f5842792f18be910f8a68907817d0");
-      // .map(result=>result.json());
-   }
-}
+  constructor(private http:HttpClient) { 
+    
+  	 console.log("service is now ready!");
+  	// this.username = 'liciolentimo';
+  }
+
+  getProfileInfo(name){
+    interface ApiData {
+      name:string;
+      avatar:string;
+      bio:string;
+      followers:number;
+      following:number;
+    }
+    let user = new User("","",0,0,"","",0,"");
+    let promise = new Promise((resolve,reject) => {
+      this.http.get<ApiData>('https://api.github.com/users/liciolentimo?access_token=addbdbd1509d13599c0df3ca811a8afdc25a7794').toPromise().then(data => {
+        user.name = data["login"];
+        user.url = data["html_url"]
+        user.created_at = data["created_at"]
+        user.repos = data["public_repos"]
+        user.avatar = data["avatar_url"];
+        user.bio = data["bio"]; 
+        user.followers = data["followers"];
+        user.following = data["following"];
+        if(data["bio"] === null) {
+          user.bio = "Github user"
+        }
+        resolve();
+      },err => {
+        user.name = "User not found";
+        reject(err);
+      })
+    })
+    return user;
+  }
+
+  getRepo(name) {
+    interface ApiData {
+      name:string;
+      description:string;
+      forks:number;
+      language:string;
+      watches:string;
+      url:string;
+      stars:number
+    }
+    let repos = [];
+    let promise = new Promise((resolve,reject) => {
+      this.http.get<ApiData>('https://api.github.com/users/liciolentimo/repos?access_token=addbdbd1509d13599c0df3ca811a8afdc25a7794').toPromise().then(response => {
+        for(let i = 0; i < response["length"];i++) {
+          let newRepo = new Repository("","",0,0,"","",0)
+          newRepo.name = response[i]["name"];
+          newRepo.forks = response[i]["forks"];
+          newRepo.language = response[i]["language"];
+          newRepo.stars = response[i]["stargazers_count"];
+          newRepo.watches = response[i]["watchers"];
+          newRepo.url = response[i]["html_url"];
+          repos.push(newRepo)
+        }
+      }, err => {
+        console.log("Repo not found");
+        reject(err);
+      })
+    })
+    return repos
+  }
+
+  	//return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+  	// .map(res => res.json());
+  }
+
+  //getProfileRepos(name){
+  	//return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+  	// .map(res => res.json());
+  //}
+
+  //updateProfile(username:string){
+  //	this.username = username;
+  //}
+
+
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,132 @@
+// import { Component, OnInit } from '@angular/core';
+// import { ProfileService } from '../profile.service';
+// import {User} from "../user";
+// // import * as $ from "jquery";
+
+// @Component({
+//   selector: 'app-user',
+//   templateUrl: './user.component.html',
+//   styleUrls: ['./user.component.css']
+// })
+// export class UserComponent  {
+
+//   profile:any[];
+
+//   user:User;
+//   repos =[];
+//   username:string;
+
+//   constructor(private profileService:ProfileService) { 
+
+//     this.profileService.updateProfile(this.username);
+//   	this.profileService.getProfileInfo().subscribe(profile => {
+//       console.log(profile);
+//     });
+
+//   this.profileService.getProfile().subscribe(profile=>{
+//     console.log(profile);
+//     this.profile = profile;
+//   });
+
+// }
+
+
+
+//   // ngOnInit() {
+//   // }
+  
+//   // viewRepo:boolean = false;
+//   // users:User[] = []
+
+//   // searchUser() {
+//   //   let userName = $("#name").val()
+//   //   this.user = this.searcher.getUser(userName);
+//   //   this.repos = this.searcher.getRepo(userName);
+//   //   $("#name").val("")
+//   //   this.viewRepo = false;
+//   //   console.log(this.repos)
+//   // }
+//   // searchRepo() {
+//   //   let toSearch = $("#repoSearch").val();
+//   //   this.repos.forEach(element => {
+//   //     element.name.indexOf(toSearch) === -1 ? element.display = false : element.display = true;
+//   //   });
+//   //   $("#repoSearch").val("")
+//   // }
+ 
+
+// // }
+
+
+
 import { Component, OnInit } from '@angular/core';
+
+
+import { User } from '../user';
+import { ProfileService } from '../profile.service';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
+  providers:[ProfileService],
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  title = 'Gitsearch';
+ 
 
-  constructor() { }
+  user:User;
+  // profile:any[];
+  repos =[];
+  clone = [];
+  username:string;
+  viewRepo:boolean = false;
 
   ngOnInit() {
   }
 
+  constructor(private profileService:ProfileService) { }
+  users:User[] = []
+
+    searchUser(){
+
+      let userName = $("#name").val()
+    this.user = this.profileService.getProfileInfo(userName);
+    this.repos = this.profileService.getRepo(userName);
+    this.clone = this.repos;
+    $("#name").val("")
+    this.viewRepo = false;
+    console.log(this.repos)
+  }
+
+  toggleRepos() {
+    this.viewRepo = !this.viewRepo;
+  }
+
+  searchRepo() {
+    let toSearch = $("#repoSearch").val();
+    this.repos.forEach(element => {
+      element.name.indexOf(toSearch) === -1 ? element.display = false : element.display = true;
+    });
+    $("#repoSearch").val("")
+    
+  }
+  viewAll() {
+    this.repos.forEach(repo => {
+      repo.display = true;
+    })
+
+    // this.profileService.updateProfile(this.username);
+  	// this.profileService.getProfileInfo().subscribe(profile => {
+    //   console.log(profile);
+    // });
+    // this.profileService.getProfileRepos().subscribe(repos => {
+    //   console.log(repos);
+    // })
 }
+
+  
+  
+}
+
